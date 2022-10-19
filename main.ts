453da3921764e1d77e93eb9bff201288f58c62ce
Javiererrejon <113448778+Javiererrejon@users.noreply.github.com>
2022-10-19 08:44:17 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,45 @@
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (randomPick == 1) {
+        info.changeScoreBy(1)
+        info.startCountdown(20)
+    }
+    generate()
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (randomPick == 0) {
+        info.changeScoreBy(1)
+        info.startCountdown(20)
+    }
+    generate()
+})
+function generate () {
+    randomPick = randint(0, 1)
+    if (randomPick == 0) {
+        mySprite.sayText("push A")
+    } else {
+        mySprite.sayText("push B")
+    }
+}
+let randomPick = 0
+let mySprite: Sprite = null
+game.splash("Push A or B as Directed")
+mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . c c c c . . . 
+    . . . . . . . c c d d d d c . . 
+    . . . . . c c c c c c d d c . . 
+    . . . c c c 4 4 4 4 d c c c c c 
+    . . c 4 4 1 4 4 4 4 4 1 c c 4 f 
+    . c 4 4 4 4 1 4 4 4 4 d 1 f 4 f 
+    f 4 4 4 4 4 1 4 4 4 4 4 1 f 4 f 
+    f 4 4 f 4 4 1 4 c f 4 4 1 4 4 f 
+    f 4 4 4 4 4 1 c 4 f 4 4 1 f f f 
+    . f 4 4 4 4 1 4 4 f 4 4 d f . . 
+    . . f 4 4 1 4 c c 4 4 d f . . . 
+    . . . f d 4 4 4 4 4 4 c f . . . 
+    . . . . f f 4 4 4 4 c d b c . . 
+    . . . . . . f f f f d d d c . . 
+    . . . . . . . . . . c c c . . . 
+    `, SpriteKind.Player)
+info.startCountdown(20)
+generate()
